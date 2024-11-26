e1610ea5f4716427523165169963b645924970cf
Thomas Nguyen <thomaswn@uci.edu>
2024-11-25 23:59:52 -0800

    Change profile picture

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "../assets/styles/Main.scss";
+import profilePic from "../assets/images/Thomas.png";
 
 function Main() {
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img
-            src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg"
-            alt="Avatar"
-          />
+          <img src={profilePic} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
